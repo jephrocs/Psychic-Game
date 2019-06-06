d99ffc42946756019376c8dec975774f4d1f9d6f
Jeff <jpconwi@gmail.com>
2019-06-06 13:36:41 -0700

    fixed bug

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,9 +11,6 @@ document.onkeyup = function (event) {
 
     var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 
-    var options = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
-
-
     if (userGuess === computerGuess) {
         wins++;
         numGuesses = 9;
