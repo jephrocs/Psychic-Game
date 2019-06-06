d12b009bce4393310b86e172f86a56d46827d172
Jeff <jpconwi@gmail.com>
2019-06-06 10:21:15 -0700

    gave javascipt its own page from html

---
+++ assets/javascript/game.js
@@ -0,0 +1,51 @@
+var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
+
+var wins = 0;
+var losses = 0;
+var numGuesses = 9;
+var guesses = [];
+
+document.onkeyup = function (event) {
+
+    var userGuess = event.key;
+
+    var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+
+    var options = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
+
+
+    if (userGuess === computerGuess) {
+        wins++;
+        numGuesses = 9;
+        guesses = [];
+    }
+
+    if (userGuess != computerGuess) {
+        numGuesses--;
+        guesses.push(userGuess);
+    }
+
+    if (numGuesses === 0) {
+
+        numGuesses = 9;
+        losses++;
+        guesses = [];
+
+
+    }
+
+
+
+    var result =
+        "<h1> The Psychic Game </h1>" +
+        "<p>Guess what letter I'm thinking of!</p>" +
+        "<p>Wins: " + wins + "</p>" +
+        "<p>Losses: " + losses + "</p>" +
+        "<p>Guesses Left: " + numGuesses + "</p>" +
+        "<p>Your Guesses so far: " + guesses.join(", ") + "</p>";
+
+    document.querySelector("#game").innerHTML = result;
+
+
+
+};
